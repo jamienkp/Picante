7a83dde31b1ba7b4de2fdf63558457094f4bca62
Jamie Ng Kwet Pin <jamie.ng@bdo.mu>
2025-04-02 11:25:39 +0400

    fix path

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from flask import Flask, render_template
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Define a route for the home page
 @app.route('/')
 def home():
-    return render_template('index.html')  # Render the index.html file
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/recipe')
+def recipe():
+    return render_template('recipe.html')
 
 # Run the Flask application
 if __name__ == '__main__':
